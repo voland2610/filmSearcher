1a121f8b3538464a3554b37c21cc6966618e61a7
voland2610 <vvoland2610@gmail.com>
2025-04-19 09:08:37 +0300

    refactor filmCard and InputSearch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import FilmCard from './FilmCard/FilmCard'
 import HeaderFilmSite from './HeaderFilmSite/HeaderFilmSite'
+import InputSearchFilm from './InputSearchFilm/InputSearchFilm'
+import { useState } from 'react';
 
 function App() {
+  const [filmData, setFilmData] = useState(null);
   return (
     <>                                                                               
       <HeaderFilmSite></HeaderFilmSite>
-      <FilmCard></FilmCard>
+      <InputSearchFilm setFilmData={setFilmData}></InputSearchFilm>
+      <FilmCard filmData={filmData}></FilmCard>
     </>
   )
 }
 
-export default App
+export default App;
